b1e4f960e5c13bda29bf5ee803e8498f9c591fcd
Nathaniel Wesley Filardo <nwf@smaug.priv.ietfng.org>
2010-02-21 01:50:41 -0500

    Add progenv/types.h (should have been earlier)

---
+++ progenv/types.h
@@ -0,0 +1,12 @@
+#ifndef _PROGENV_TYPES_H_
+#define _PROGENV_TYPES_H_
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef int8_t s8;
+typedef uint16_t s16;
+typedef uint32_t u32;
+
+#endif
